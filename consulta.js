eb9c70d17d7054bb2d02e5666731d9ab2b39223a
Murilo Soares <murilosoaresc3@gmail.com>
2022-04-14 10:45:57 -0300

    comentários adicionados

--- consulta.js
+++ consulta.js
@@ -1,12 +1,15 @@
+//busca os dados da API
 function consultaApi() {
     fetch('https://mhw-db.com/ailments')
     .then(response => response.json())
     .then(data => {
         const lista = document.querySelector('#lista-debuffs')
 
+        //passa por todos os itens do array, cria os itens de lista, insere as informações e adiciona os itens dentro da lista
         data.map((debuff) => {
             const li = document.createElement('li');
 
+            //popula a li
             li.setAttribute('id', debuff.id);
             li.innerHTML = debuff.name;
             li.innerHTML = debuff.description;
